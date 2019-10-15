3aaca517ae9ee95e4658fb57fa8991ff61c2096b
Jon Tvermose <jon.tvermose.nielsen@gmail.com>
2019-10-15 19:28:23 +0200

    Stuff?

---
+++ PodioSyncer/client/components/podio-row/podio-row.tsx
@@ -0,0 +1,35 @@
+﻿import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+
+type PodioRowProps = {
+    name: string,
+    appId: number,
+    webhookUrl: string,
+    status: boolean,
+    verified: boolean,
+    onEdit(appId: number): void,
+    onDelete(appId: number): void
+}
+
+const StatusDiv = styled.div<{ active: boolean }>`
+background-color: ${(props) => props.active ? "green" : "red"};
+width: 20px;
+height: 20px;
+border-radius: 10px;
+`;
+
+export const PodioRow: FunctionComponent<PodioRowProps> = (podioProps) => {
+
+
+
+    return (
+        <div className="row">
+            <div className="col-3">{podioProps.name}</div>
+            <div className="col-2">{podioProps.appId}</div>
+            <div className="col-3">{podioProps.webhookUrl}</div>
+            <div className="col-1"><StatusDiv active={podioProps.status} /></div>
+            <div className="col-1"><StatusDiv active={podioProps.verified} /></div>
+            <div className="col-1" onClick={() => podioProps.onEdit(podioProps.appId)}>Edit</div>
+            <div className="col-1" onClick={() => podioProps.onDelete(podioProps.appId)}>Delete</div>
+        </div>)
+}
